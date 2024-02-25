0938eae7f8e91f0f4f702efddb442201fb67cf93
Frank DiGiacomo <frankied003@hotmail.com>
2024-02-24 21:19:34 -0500

    Frankied.dev is now public

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,6 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link rel="stylesheet" href="https://use.typekit.net/abc6rqq.css"></link>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
